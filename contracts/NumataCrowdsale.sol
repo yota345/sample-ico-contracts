a40c3a27186a875f4748029eecad64157ae1a438
takuwan0405 <artsplaying.for496@gmail.com>
2018-01-03 15:07:47 +0900

    Add sample crowdsale

---
+++ contracts/NumataCrowdsale.sol
@@ -0,0 +1,34 @@
+pragma solidity ^0.4.17;
+
+import 'zeppelin-solidity/contracts/crowdsale/CappedCrowdsale.sol';
+import 'zeppelin-solidity/contracts/crowdsale/RefundableCrowdsale.sol';
+import 'zeppelin-solidity/contracts/lifecycle/Pausable.sol';
+import 'zeppelin-solidity/contracts/token/MintableToken.sol';
+import './NumataToken.sol';
+
+contract NumataCrowdsale is CappedCrowdsale, RefundableCrowdsale, Pausable {
+
+    function NumataCrowdsale(
+        uint256 _startTime,
+        uint256 _endTime,
+        uint256 _rate,
+        uint256 _goal,
+        uint256 _cap,
+        address _wallet
+    )
+        public
+        CappedCrowdsale(_cap)
+        FinalizableCrowdsale()
+        RefundableCrowdsale(_goal)
+        Crowdsale(_startTime, _endTime, _rate, _wallet)
+    {
+        //As goal needs to be met for a successful crowdsale
+        //the value needs to less or equal than a cap which is limit for accepted funds
+        require(_goal <= _cap);
+    }
+
+    function createTokenContract() internal returns (MintableToken) {
+        return new NumataToken();
+    }
+
+}
