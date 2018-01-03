a01eca0353b23ac08c8ea2dd19bb27b2b9a76516
takuwan0405 <artsplaying.for496@gmail.com>
2018-01-03 21:11:47 +0900

    Add eslint

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,25 +1,25 @@
 const fs = require('fs');
+
 const NumataCrowdsale = artifacts.require('NumataCrowdsale.sol');
 const NumataFund = artifacts.require('NumataFund.sol');
 const crowdsaleParams = JSON.parse(fs.readFileSync('../config/crowdsale.json', 'utf8'));
 const fundParams = JSON.parse(fs.readFileSync('../config/fund.json', 'utf8'));
 
 module.exports = function deployContracts(deployer) {
-    const cap = web3.toWei(crowdsaleParams.cap, 'ether');
-    const initialFundBalance = web3.toWei(crowdsaleParams.initialFundBalance, 'ether');
-    const goal = web3.toWei(crowdsaleParams.goal, 'ether');
-    const endTime = crowdsaleParams.startTime + (86400 * crowdsaleParams.icoDays)
+  const cap = web3.toWei(crowdsaleParams.cap, 'ether');
+  const initialFundBalance = web3.toWei(crowdsaleParams.initialFundBalance, 'ether');
+  const goal = web3.toWei(crowdsaleParams.goal, 'ether');
+  const endTime = crowdsaleParams.startTime + (86400 * crowdsaleParams.icoDays);
 
-    deployer.deploy(NumataFund, fundParams.owners, fundParams.required).then(() =>
-        deployer.deploy(NumataCrowdsale,
-            crowdsaleParams.startTime,
-            endTime,
-            crowdsaleParams.rate.base,
-            goal,
-            cap,
-            initialFundBalance,
-            NumataFund.address
-        )
-    );
-
+  deployer.deploy(NumataFund, fundParams.owners, fundParams.required).then(() =>
+    deployer.deploy(
+      NumataCrowdsale,
+      crowdsaleParams.startTime,
+      endTime,
+      crowdsaleParams.rate.base,
+      goal,
+      cap,
+      initialFundBalance,
+      NumataFund.address,
+    ));
 };
